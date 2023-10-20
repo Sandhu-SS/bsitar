bf41d21dc7431fc3daa412684c3ac588c47f3baa
Sandhu-SS <satpal.sandhu@bristol.ac.uk>
2023-10-20 12:38:45 +0530

    update

--- R/growthparameters_comparison.R
+++ R/growthparameters_comparison.R
@@ -5,12 +5,23 @@
 
 #' Compute growth parameters for the \code{bgmfit} model
 #' 
-#'@description The \code{comparisons_bgm} provides estimates of growth 
-#'  parameters (such as peak velocity and the age at peak velocity) derived 
-#'  from the growth velocity curve. 
+#'@description The \code{growthparameters_comparison} function provides
+#'  estimates of growth parameters (such as peak velocity and the age at peak
+#'  velocity) derived from the growth velocity curve. This function is a wrapper
+#'  around the [marginaleffects::comparisons()] and
+#'  [marginaleffects::avg_comparisons()]. These function estimate and return
+#'  predicted values at t different regressor values and and compare those
+#'  predictions by computing a difference between them. The
+#'  [marginaleffects::comparisons()] computes unit-level (conditional) estimates
+#'  whereas [marginaleffects::avg_comparisons()] return average (marginal)
+#'  estimates. A detailed explanation is available at
+#'  <https://marginaleffects.com/articles/comparisons.html> and
+#'  <https://marginaleffects.com/>. Note that for the current use case, i.e.,
+#'  for estimating growth parameters, the arguments, especially \code{variables}
+#'  and \code{comparion} are modified via custom functions (see below).
 #'  
-#' @details The \code{comparisons_bgm} function estimates and returns the 
-#' following growth paramaeter:
+#' @details The \code{growthparameters_comparison} function estimates and
+#'   returns the following growth paramaeters:
 #' \itemize{
 #'   \item pv  - peak velocity
 #'   \item apv - age at peak velocity
@@ -29,7 +40,7 @@
 #' takes a real value as an input (greater than 0 and less than 1). Typically, a
 #' 10 percent of \code{pv} (i.e., \code{acg_velocity = 0.1}) is considered as a
 #' good indicator of the cessation of the active pubertal growth spurt
-#' (\insertCite{Anna2022}{bsitar}).
+#' \insertCite{Anna2022}{bsitar}.
 #' 
 #' 
 #' @param model An object of class \code{bgmfit}.
@@ -39,19 +50,42 @@
 #' \code{vto}, \code{vpv}, \code{apv}, \code{vcg}, and \code{acg}. For 
 #' \code{parameter = NULL} (default), age at peak growth velocity is estimated.
 #' 
-#' @param acg_velocity A real number to set the percentage of the peak growth 
-#' as the cessation velocity. The \code{acg_velocity} should be greater than 
-#' \code{0} and less than \code{1}. The default \code{acg_velocity = 0.10}
-#' indicates that a 10 per cent of the peak growth velocity will be used to 
-#' get the cessation velocity and the age at the cessation velocity. For 
-#' example is peak growth velocity is estimated as \code{10 mm/year}, then 
-#' cessation velocity would be \code{1 mm/year}. 
+#' @param acg_velocity A real number to set the percentage of peak growth growth
+#'   velocity as the cessation velocity when estimating the \code{vcg} and
+#'   \code{acg} growth parameters. The \code{acg_velocity} should be greater
+#'   than \code{0} and less than \code{1}. The default \code{acg_velocity =
+#'   0.10} indicates that a 10 per cent of the peak growth velocity will be used
+#'   to get the cessation velocity and the corresponding age at the cessation
+#'   velocity. For example if peak growth velocity estimate is \code{10
+#'   mm/year}, then cessation velocity is \code{1 mm/year}.
 #' 
-#' @param digits An integer (default \code{2}) to set the rounding disgits.
+#' @param digits An integer (default \code{2}) to set the decimal places for the
+#'   estimated growth parameters. The \code{digits} is passed on to the
+#'   [base::round()] function.
+#'   
+#' @param average A logical to indicate whether to internally call the
+#'    [marginaleffects::comparisons()] or the
+#'    [marginaleffects::avg_comparisons()] function. If \code{FALSE} (default),
+#'    [marginaleffects::comparisons()] is called otherwise
+#'    [marginaleffects::avg_comparisons()] when \code{average = TRUE}.
+#'    
+#' @param variables For estimating growth parameters in the current use case, 
+#' the \code{variables} is the level 1 predictor such as \code{age}/\code{time}
+#' and is is a named list with value of list set via the \code{esp} 
+#' (default 1e-6). If \code{NULL}, the \code{variables} is set internally by
+#' retrieving the relevant information from the \code{model}. Otherwise, 
+#' can define it as \code{variables = list('age' = 1e-6)}.
+#' 
+#' @param comparison For estimating growth parameters in the current use case,
+#' the \code{comparison} is an internal function that uses [sitar::getPeak()], 
+#' [sitar::getTakeoff()] and [sitar::getTrough()] functions to estimate
+#' various growth parameters. 
+#' 
 #' 
 #' 
 #' @inheritParams  growthparameters.bgmfit
 #' @inheritParams  marginaleffects::comparisons
+#' @inheritParams  marginaleffects::avg_comparisons
 #'
 #' @return A data frame objects with estimates and CIs for computed parameter(s)
 #' @export
@@ -61,10 +95,10 @@
 #' 
 #' @examples
 #' \dontrun{
-#' comparisons_bgm(model, parameter = 'apv')
+#' growthparameters_comparison(model, parameter = 'apv')
 #' }
 #' 
-comparisons_bgm.bgmfit <- function(model,
+growthparameters_comparison.bgmfit <- function(model,
                                    resp = NULL,
                                    ndraws = NULL,
                                    draw_ids = NULL,
@@ -83,10 +117,10 @@ comparisons_bgm.bgmfit <- function(model,
                                    future = FALSE,
                                    future_session = 'multisession',
                                    cores = NULL,
+                                   average = FALSE, 
                                    variables = NULL,
                                    comparison = "difference",
                                    type = NULL,
-                                   vcov = TRUE,
                                    by = FALSE,
                                    conf_level = 0.95,
                                    transform = NULL,
@@ -94,8 +128,6 @@ comparisons_bgm.bgmfit <- function(model,
                                    wts = NULL,
                                    hypothesis = NULL,
                                    equivalence = NULL,
-                                   p_adjust = NULL,
-                                   df = Inf,
                                    eps = NULL,
                                    ...) {
   if (is.null(ndraws))
@@ -296,7 +328,11 @@ comparisons_bgm.bgmfit <- function(model,
     comparisons_arguments$comparison <- gparms_fun
     
     suppressWarnings({
-      out <- do.call(marginaleffects::comparisons, comparisons_arguments)
+      if(!average) {
+        out <- do.call(marginaleffects::comparisons, comparisons_arguments)
+      } else if(average) {
+        out <- do.call(marginaleffects::avg_comparisons, comparisons_arguments)
+      }
     })
     
     out
@@ -344,16 +380,16 @@ comparisons_bgm.bgmfit <- function(model,
 
 
 
-#' @rdname comparisons_bgm.bgmfit
+#' @rdname growthparameters_comparison.bgmfit
 #' @export
-comparisons_bgm <- function(model, ...) {
-  UseMethod("comparisons_bgm")
+growthparameters_comparison <- function(model, ...) {
+  UseMethod("growthparameters_comparison")
 }
 
 
 # setmodel <- berkeley_fit
 #
-# # comparisons_bgm(setmodel, parameter = allowed_parms)
+# # growthparameters_comparison(setmodel, parameter = allowed_parms)
 #
 # allowed_parms <- c('ato', 'vto', 'apv', 'vpv', 'afo', 'vfo')
 #
@@ -361,7 +397,7 @@ comparisons_bgm <- function(model, ...) {
 # list_cout <- list()
 # list_name <- list()
 # for (allowed_parmsi in allowed_parms) {
-#   cout <- comparisons_bgm(setmodel, re_formula = NA, parameter = allowed_parmsi,
+#   cout <- growthparameters_comparison(setmodel, re_formula = NA, parameter = allowed_parmsi,
 #                        newdata = newdata)
 #   list_name[[allowed_parmsi]] <- allowed_parmsi
 #   list_cout[[allowed_parmsi]] <- cout
