4a4b76b60b80ca7f47124c2b557b23456cbbef94
Sandhu-SS <satpal.sandhu@bristol.ac.uk>
2024-10-09 08:52:25 +0530

    update

--- R/marginal_draws.R
+++ R/marginal_draws.R
@@ -606,40 +606,44 @@ marginal_draws.bgmfit <-
     
     
     
-    need_future_re_expose_cpp <- FALSE
-    if(any(grepl("pstream__",
-                 deparse(model$model_info$exefuns[[1]])))) {
-      need_future_re_expose_cpp <- TRUE
-    }
-    
-    re_expose <- FALSE
-    if(is.null(future_re_expose)) {
-      if(setplanis == "multisession") {
-        if(need_future_re_expose_cpp) {
+    if (future) {
+      need_future_re_expose_cpp <- FALSE
+      if(any(grepl("pstream__",
+                   deparse(model$model_info$exefuns[[1]])))) {
+        need_future_re_expose_cpp <- TRUE
+      }
+      
+      re_expose <- FALSE
+      if(is.null(future_re_expose)) {
+        if(setplanis == "multisession") {
+          if(need_future_re_expose_cpp) {
+            re_expose <- TRUE
+            if(verbose) {
+              message("For multisession plan, argument 'future_re_expose' has been set as TRUE")
+            }
+          } else if(!need_future_re_expose_cpp) {
+            if(verbose) {
+              message("To speed up the calulations, it is advised to set future_re_expose = TRUE")
+            }
+          }
+        }
+      } else if(!is.null(future_re_expose)) {
+        if(future_re_expose) {
           re_expose <- TRUE
-          message("For multisession plan, argument 'future_re_expose' has been set as TRUE")
-        } else if(!need_future_re_expose_cpp) {
-          if(verbose) {
-            message("To speed up the calulations, it is advised to set future_re_expose = TRUE")
+        } else if(!future_re_expose) {
+          if(!need_future_re_expose_cpp) {
+            # if(expose_method_set == "R") {
+            if(verbose) {
+              message("To speed up the calulations, it is advised to set future_re_expose = TRUE")
+            }
+          } 
+          if(need_future_re_expose_cpp & setplanis == "multisession") {
+            # if(expose_method_set != "R") {
+            stop("For plan multisession, the functions need to be re_exposed by setting future_re_expose = TRUE")
           }
         }
       }
-    } else if(!is.null(future_re_expose)) {
-      if(future_re_expose) {
-        re_expose <- TRUE
-      } else if(!future_re_expose) {
-        if(!need_future_re_expose_cpp) {
-        # if(expose_method_set == "R") {
-          if(verbose) {
-            message("To speed up the calulations, it is advised to set future_re_expose = TRUE")
-          }
-      } 
-      if(need_future_re_expose_cpp & setplanis == "multisession") {
-        # if(expose_method_set != "R") {
-          stop("For plan multisession, the functions need to be re_exposed by setting future_re_expose = TRUE")
-        }
-      }
-    }
+    } # if (future) {
     
     
     
